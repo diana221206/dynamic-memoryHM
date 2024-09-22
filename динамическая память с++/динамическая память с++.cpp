84397f0d6f946223ffdd7356b51fb79319454519
Admin <Admin@DESKTOP-FNBVACD>
2024-09-22 23:49:02 +0300

    Добавьте файлы проекта.

---
+++ динамическая память с++/динамическая память с++.cpp
@@ -0,0 +1,34 @@
+﻿// динамическая память с++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+
+    int* array1 = new int[size];
+
+    std::cout << "Введите " << size <<
+        "элементов массива:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cin >> array1[i];
+    }
+    std::cout << "Массив 1:";
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    array2[size] = 100;
+    delete[] array1;
+
+    std::cout << "Массив 2: ";
+    for (int i = 0; i < size + 1; ++i)
+    {
+
+        std::cout << array2[i] << " ";
+    }
+    std::cout << std::endl;
+    delete[] array2;
+    return 0;
